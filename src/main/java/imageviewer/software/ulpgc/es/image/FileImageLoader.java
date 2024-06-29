094c027cb2ca64c8de10849f30dcce82bedc799b
XxMARRExX <xx_marre_xx@hotmail.com>
2024-06-29 09:09:48 +0100

    Implementation of classes FileImageLoader and Resizer

---
+++ src/main/java/imageviewer/software/ulpgc/es/image/FileImageLoader.java
@@ -0,0 +1,49 @@
+package imageviewer.software.ulpgc.es.image;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Set;
+
+public class FileImageLoader implements ImageLoader {
+    private final File[] files;
+
+    public FileImageLoader(File folder) {
+        this.files = folder.listFiles(isImage());
+    }
+
+    private static final Set<String> imageExtensions = Set.of(".jpg", ".png", ".jpeg", ".bmp", ".tiff", ".gif");
+    private static FilenameFilter isImage() {
+        return (dir, name) -> imageExtensions.stream().anyMatch(name::endsWith);
+    }
+    @Override
+    public Image load() {
+        return imageAt(0);
+    }
+
+    private Image imageAt(int i) {
+        return new Image() {
+            @Override
+            public String name() {
+                assert files != null;
+                return files[i].getAbsolutePath();
+            }
+
+            @Override
+            public Image prev() {
+                assert files != null;
+                return imageAt((i + 1) % files.length);
+            }
+
+            @Override
+            public Image next() {
+                assert files != null;
+                return imageAt(i > 0 ? i - 1 : files.length - 1);
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return obj instanceof Image && ((Image) obj).name().equals(this.name());
+            }
+        };
+    }
+}
